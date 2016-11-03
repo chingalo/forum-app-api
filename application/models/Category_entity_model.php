22343cf71e6e66aa4ba78e605325a49ec391e123
Joseph Chingalo <profschingalo@gmail.com>
2016-11-03 10:18:55 +0300

    rename some columns on category entity get functionality

--- application/models/Category_entity_model.php
+++ application/models/Category_entity_model.php
@@ -19,7 +19,7 @@ class Category_entity_model extends CI_Model
                 $ids = $ids ."," .$categoryId[$index];
             }
         }
-        $sql = "SELECT categories_entity.eid, categories_entity.cat_id, categories_entity.title, categories_entity.description, categories_entity.posted_date, users.username, COUNT(comments.comment_id) AS numberOfComents FROM categories_entity JOIN users ON users.user_id = categories_entity.user_id JOIN comments ON comments.eid = categories_entity.eid WHERE categories_entity.cat_id IN('".$ids."') GROUP BY categories_entity.eid";
+        $sql = "SELECT categories_entity.eid AS id , categories_entity.cat_id, categories_entity.title, categories_entity.description, categories_entity.posted_date, users.username AS poster, COUNT(comments.comment_id) AS numberOfComents FROM categories_entity JOIN users ON users.user_id = categories_entity.user_id JOIN comments ON comments.eid = categories_entity.eid WHERE categories_entity.cat_id IN('".$ids."') GROUP BY categories_entity.eid";
         $result = $this->db->query($sql);
         return $result->result();
 
